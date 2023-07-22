522e31f56d4cb8c27c0fc87daff7bd261e23b5eb
Eyal Lampel <eyal.lampel@gmail.com>
2023-07-22 09:33:33 +0300

    fixed the bugs in server and clinet , now we need to add DB functionallty

--- ip_status_app/server.js
+++ ip_status_app/server.js
@@ -1,50 +1,46 @@
-let failFlag=true;
-
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const Ping = require("ping-wrapper");
 
 const app = express();
-
-app.use((req, res, next) => {
-    res.set('Cache-Control', 'no-store')
-    next()
-  })
-
-  
 app.use(cors());
-
-// Add middleware to parse JSON from requests
 app.use(express.json());
 
-var Ping = require('ping-wrapper');
+app.use((req, res, next) => {
+  res.set("Cache-Control", "no-store");
+  next();
+});
 
-
-// load configuration from file 'config-default-' + process.platform
-// Only linux is supported at the moment
 Ping.configure();
 
+const hosts = {
+  "7.0.0.1": "Host1",
+  "127.0.0.1": "Host2",
+  "9.0.0.1": "Host3",
+  // Add more hosts here
+};
 
-var ping = new Ping('777.46.0.6');
+const pingHost = (ip, hostname) => {
+  return new Promise((resolve, reject) => {
+    let ping = new Ping(ip);
 
-ping.on('ping', function(data){
-	console.log('Ping %s: time: %d ms', data.host, data.time);
-  failFlag=false;
-});
+    ping.on("ping", (data) => {
+      resolve({ ipAddress: ip, hostname: hostname, ipReplay: true });
+    });
 
-ping.on('fail', function(data){
-	console.log('Fail', data);
-  failFlag=true;
-});
-
-
-
-app.get('/api/ipstatus/', (req, res) => {
-    // simulate dynamic data
-    const data = ping; // Function that dynamically generates new data
-  
-    res.json(failFlag);
+    ping.on("fail", (data) => {
+      resolve({ ipAddress: ip, hostname: hostname, ipReplay: false });
+    });
   });
+};
+
+app.get("/api/ipstatus/", async (req, res) => {
+  let promises = Object.entries(hosts).map(([ip, hostname]) =>
+    pingHost(ip, hostname),
+  );
+  let results = await Promise.all(promises);
+  res.json(results);
+});
 
 const PORT = process.env.PORT || 4900;
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // Debug log
--- ip_status_app/src/App.js
+++ ip_status_app/src/App.js
@@ -1,47 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-let myObj={};
-
-
   const [ipData, setIpData] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:4900/api/ipstatus')
-      .then((res) => {
-        console.log("connected!!!"); // Debug log
-        if (!res.ok) {
-          throw new Error(`HTTP error! status: ${res.status}`);
-        }
-        return res.json();
-      })
-      .then((data) => {
-        console.log('Data fetched from server:', data); // Debug log
-        if (data.status === false){
-          myObj.status = true;
-        }
-        else{
-          myObj.status = false;
-        }
+    const fetchData = () => {
+      fetch("http://localhost:4900/api/ipstatus")
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`);
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setIpData(data);
+        })
+        .catch((error) => {
+          console.log("Fetching data failed:", error); // Debug log
+        });
+    };
 
-        setIpData(myObj);
-      })
-      .catch((error) => {
-        console.log('Fetching data failed:', error); // Debug log
-      });
+    fetchData(); // Fetch data immediately when the component is mounted
+
+    const intervalId = setInterval(fetchData, 5000); // Fetch data every 5 seconds
+
+    // Clean up function: Clear the interval when the component is unmounted
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
     <div className="App">
       {ipData.map((item, index) => (
         <div key={index}>
-          <p>{item.ip}</p>
-          <p className={item.status ? 'online' : 'offline'}>
-            {item.status ? 'Online' : 'Offline'}
+          <p>
+            {item.hostname} ({item.ipAddress})
+          </p>
+          <p className={item.ipReplay ? "online" : "offline"}>
+            {item.ipReplay ? "Online" : "Offline"}
           </p>
         </div>
       ))}
+      b
     </div>
   );
 }
